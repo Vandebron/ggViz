e5fb9a7d9379abc4b2b76078991c53c9786edec9
cvmartin <cvarela@gmx.at>
2020-11-07 18:42:16 +0100

    rename

--- R/app_server.R
+++ R/app_server.R
@@ -7,29 +7,29 @@ app_server <- function(input, output, session) {
   
   cnf <- config::get(file = get_inst_file("config.yml"))
   
-  r_plot <- reactiveValues(
+  r_data <- reactiveValues(
     initial = NULL, 
     final = NULL
   )
   
   
   # INPUT ------------------------------------------------------------------------
-  mod_import_data_server("x", r = r_plot)
+  mod_import_data_server("x", r = r_data)
   
-  mod_vars_cont_fcts_server("x", r = r_plot)
+  mod_vars_cont_fcts_server("x", r = r_data)
   
 
   
   # BUILD CODE ----------------------------------------------------------
   
-  code_graph <- mod_gg_layers_server("x", r = r_plot)
+  code_graph <- mod_gg_layers_server("x", r = r_data)
   
   
   # OUTPUT -----------------------------------------------------------------------
   # table -------
   output$out_table <- DT::renderDT({
     DT::datatable(
-      r_plot$final$df,
+      r_data$final$df,
       rownames = FALSE,
       class = "row-border",
       options = list(
@@ -47,7 +47,7 @@ app_server <- function(input, output, session) {
     
     rlang::eval_tidy(
       rlang::parse_expr(input$ace_graph), 
-      data = list(df = r_plot$final$df), 
+      data = list(df = r_data$final$df), 
       env = generate_safe_env()
     )
   })
@@ -76,9 +76,9 @@ app_server <- function(input, output, session) {
   # output$text <- renderText({
   #   jsonlite::toJSON(
   #     list(
-  #       df_zero = r_plot$df_zero,
-  #       df_initial = r_plot$df_inital,
-  #       df_final = r_plot$df_final
+  #       df_zero = r_data$df_zero,
+  #       df_initial = r_data$df_inital,
+  #       df_final = r_data$df_final
   #     )
   #   )
   # })
